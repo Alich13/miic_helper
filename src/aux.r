3a3007df1c07b778d35f57ee3806b41e96f8a0f5
Alich13 <chemkhi.ali13@gmail.com>
2025-03-04 13:52:19 +0100

    * helper function to preprocess seurat object for miic analysis

---
+++ src/aux.r
@@ -0,0 +1,222 @@
+
+
+# convert from Seurat to anndata format https://smorabit.github.io/blog/2021/velocyto/
+
+library(Seurat)
+
+
+
+average_gene_count <- function(pattern,Object,name){
+  
+  pattern_genes <- rownames(Object)[grepl(pattern, rownames(Object), ignore.case = TRUE)]
+  pattern_matrix <- GetAssayData(Object, layer = "count")[pattern_genes,]
+  mean_expression <- colMeans(pattern_matrix)
+  
+  Object <- AddMetaData(
+    object = Object,
+    metadata = mean_expression, # Mean expression 
+    col.name = name )
+  
+  #Remove the original ribosomal genes
+  remaining_genes <- setdiff(rownames(Object@assays$RNA$counts), c(pattern_genes))
+  Object <- subset(Object, features = remaining_genes)
+  
+  return(Object)
+}
+
+
+load_seurat_data <- function(data_dir) {
+  
+  # Load expression matrix
+  counts <- readMM(file.path(data_dir,"matrix.mtx"))
+  rownames(counts) <- readLines(file.path(data_dir,"features.tsv.gz"))  # Assign gene names
+  colnames(counts) <- readLines(file.path(data_dir,"barcodes.tsv.gz"))  # Assign cell barcodes
+  
+  # Check if metadata is provided
+  metadata_file<-file.path(data_dir,"metadata.txt")
+  if (!is.null(metadata_file)) {
+    metadata <- read.table(metadata_file, sep="\t", header=TRUE, row.names=1)
+    seurat_obj <- CreateSeuratObject(counts = counts, meta.data = metadata)
+  } else {
+    seurat_obj <- CreateSeuratObject(counts = counts)
+  }
+  
+  seurat_obj@meta.data$orig.ident=seurat_obj@meta.data$myIdent
+  seurat_obj<-SetIdent(seurat_obj, value = "orig.ident")
+  
+  return(seurat_obj)
+}
+
+
+
+# Parse spliced RNA counts 
+myParseUnsplicedSO <- function(dir, sample_name){
+  # Load the matrix
+  matrix_data <- readMM(file.path(dir,"unspliced.mtx"))
+  # Load the barcodes
+  barcodes <- readLines(file.path(dir,"unspliced.barcodes.txt"))
+  # Load the gene names
+  genes <- read.table(file.path(dir,"unspliced.genes.txt"), header = FALSE, stringsAsFactors = FALSE)
+  
+  # Assign row and column names
+  colnames(matrix_data) <- genes$V1
+  rownames(matrix_data) <- barcodes
+  
+  # Create the Seurat object
+  seurat_obj <- CreateSeuratObject(counts = t(matrix_data), project = sample_name)
+  
+  # Print summary
+  return(seurat_obj)
+}
+
+
+# Parse spliced RNA counts 
+myParseSO <- function(dir, sample_name){
+  # Load the matrix
+  matrix_data <- readMM(file.path(dir,"spliced.mtx"))
+  # Load the barcodes
+  barcodes <- readLines(file.path(dir,"spliced.barcodes.txt"))
+  # Load the gene names
+  genes <- read.table(file.path(dir,"spliced.genes.txt"), header = FALSE, stringsAsFactors = FALSE)
+  
+  # Assign row and column names
+  colnames(matrix_data) <- genes$V1
+  rownames(matrix_data) <- barcodes
+  
+  # Create the Seurat object
+  seurat_obj <- CreateSeuratObject(counts = t(matrix_data), project = sample_name)
+  
+  # Print summary
+  return(seurat_obj)
+}
+
+
+#Function straining
+myStrainingAmbRNA <- function(exp, sample){
+  
+  raw_matrix <- file.path(exp,sample, "outs/raw_feature_bc_matrix")
+  filtered_matrix <- file.path(exp,sample,"outs/filtered_feature_bc_matrix")
+  
+  message("Matrices are being read ...")
+  toc <- Seurat::Read10X(filtered_matrix)
+  tod <- Seurat::Read10X(raw_matrix)
+  
+  srat <- CreateSeuratObject(counts = toc) # maybe set assay next time ?
+  soup.channel <- SoupChannel(tod, toc)
+  
+  # Find Clusters for SoupX
+  message("Clustering for SoupeX algorithm ...")
+  srat    <- SCTransform(srat, verbose = F)
+  srat    <- RunPCA(srat, verbose = F)
+  srat    <- RunUMAP(srat, dims = 1:30, verbose = F)
+  srat    <- FindNeighbors(srat, dims = 1:30, verbose = F)
+  srat    <- FindClusters(srat, verbose = T) # res 0.8 by default
+  
+  # Saving metadata for sanity check
+  meta <- srat@meta.data
+  umap <- srat@reductions$umap@cell.embeddings
+  soup.channel <- setClusters(soup.channel, setNames(meta$seurat_clusters, rownames(meta)))
+  soup.channel <- setDR(soup.channel, umap)
+  
+  # Calculating ambient RNA profile
+  message("Calculating ambiant RNA profile ...")
+  soup.channel <- autoEstCont(soup.channel)
+  
+  # SoupX matrix correction
+  message("SoupX matrix correction ...")
+  adj.matrix  <- adjustCounts(soup.channel, roundToInt = T) #roundToInt to make sure we output an integer matrix
+  
+  # Reincorporating Antibody Capture assay to the matrix
+  data <- list("Gene Expression" = adj.matrix)
+  
+  
+  message("Creation of Seurat object...")
+  Object <- CreateSeuratObject(counts = data$`Gene Expression`, project = sample, assay = "RNA")
+  
+  return(Object)
+}
+
+
+# Function Doublet removal
+myDoubletRemoval <- function(Object){
+  # Doublet removal
+  message("Doublet removal using scDblFinder ...")
+  sce <- SingleCellExperiment(assays=list(counts=Object@assays$RNA@counts))
+  message("SingleCellExperiment done")
+  sce <- scDblFinder(sce)
+  
+  sce_name <- paste0(project.name,'_',day,'_sce')
+  sce <- get(sce_name)
+  
+  Object<-Object[,sce@colData$scDblFinder.class == "singlet"]
+  
+  return(Object)
+  
+}
+
+myQCAnnotation <- function(Object){
+  message("Adding QC metadata to the seurat object")
+  Object[["percent.mt"]] <- PercentageFeatureSet(Object, pattern = "^mt-")
+  Object[["percent.Rps"]] <- PercentageFeatureSet(Object, pattern = "^Rps")
+  Object[["percent.Rpl"]] <- PercentageFeatureSet(Object, pattern = "^Rpl")
+  
+  return(Object)
+}
+
+
+
+myNormalizeDatabySizeFactor <- function(SeuratObject){
+  ## Normalisation dependant of the theoric size of cells (number of counts and features)
+  sce.object <- as.SingleCellExperiment(SeuratObject)
+  clusters <- quickCluster(sce.object)
+  sce.object <- computeSumFactors(sce.object, clusters = clusters)
+  sce.object <- logNormCounts(sce.object)
+  
+  # Reconverting the sce.object into a seurat object and redo the defaults idents and assays
+  SeuratObject <- as.Seurat(sce.object,)
+  DefaultAssay(SeuratObject) <- 'RNA'
+  
+  return(SeuratObject)
+}
+
+
+myEnhancedVolcano <- function(markers,
+                              pvalue_cutoff = 0.05,
+                              FC_cutoff = 0.5,
+                              onepop = "",
+                              ident.1,
+                              ident.2
+){
+  
+  
+  keyvals.color.p_val <- ifelse(
+    markers$avg_log2FC < -FC_cutoff & markers$p_val_adj < pvalue_cutoff, "royalblue" , 
+    ifelse(markers$avg_log2FC > FC_cutoff & markers$p_val_adj < pvalue_cutoff, "red2", "grey30"))
+  names(keyvals.color.p_val)[keyvals.color.p_val == "red2"] <- paste("Overexpressed genes in", ident.1, onepop)
+  names(keyvals.color.p_val)[keyvals.color.p_val == "royalblue"] <- paste("Overexpressed genes in",ident.2, onepop)
+  names(keyvals.color.p_val)[keyvals.color.p_val == "grey30"] <- "Not sig."
+  
+  
+  
+  plotDEG <- EnhancedVolcano(markers,
+                             lab = rownames(markers),
+                             x = 'avg_log2FC',
+                             y = 'p_val_adj',
+                             legendPosition = 'right',
+                             legendLabSize = 15,
+                             legendIconSize = 5,
+                             legendLabels = c(paste("Overexpressed genes in",ident.2, onepop), paste("Overexpressed genes in",ident.1, onepop), "Not sig."),
+                             pointSize = 4.5,
+                             colCustom = keyvals.color.p_val,
+                             drawConnectors = T,
+                             widthConnectors = 0.75,
+                             pCutoff = pvalue_cutoff,
+                             FCcutoff = FC_cutoff,
+                             boxedLabels = T,
+                             title = paste('DEG analysis of',ident.1, onepop, 'versus',ident.2, onepop, ' with wilcox test'),
+                             subtitle = "Bonferroni adj p-value"
+  )  +
+    theme(legend.position = "bottom")
+  
+  return(plotDEG)
+}
---
+++ src/preprocess_sureat_object.R
@@ -0,0 +1,226 @@
+############################################################################################################################################################
+# Author : achemkhi ali.chemkhi@curie.fr
+
+# This scripts preprocess a seurat object in the aim of generating input files for miic 
+# select the features that are relevant to our analysis based on a list of foi example c("gene1","gene2","metadata1")
+# all the metadata we want to include in miic must be in the seurat object 
+############################################################################################################################################################
+
+
+library(argparse)
+library(Seurat)
+library(dplyr)
+library(grid)
+library(gridExtra)
+library(fs)
+library(millefy)
+library(ggplot2)
+library(miic) #devtools::install_github("interactMIIC/causalCCC/miic_R_package", force = T, ref = "achemkhi/no-goi-limit-branch") #latest version of MIIC
+#source("src/aux.R") # TODO add the function in the same script 
+
+
+# Filter
+Filter_seurat_object <- function(seurat_object, filter_list) {
+
+  if (length(filter_list) > 0) {  # Apply filters only if filter_list is not empty
+    for (name in names(filter_list)) {
+      print(paste0("filtered ",name," colomn. keeping only ",paste(filter_list[[name]],collapse = " and ") ))
+      Idents(seurat_object)<-name
+      print(name)
+      seurat_object <- subset(seurat_object, idents = filter_list[[name]])
+    }
+  }
+  
+  # A workaround to make MIselection function work. Because it needs a cell type filter 
+  seurat_object$myIdentFilter<-"oneinteract"
+  
+  return(seurat_object)
+}
+
+
+# Heatmap
+MI_heatmap <- function(heatmap_selection,highlight_genes,plot_name,output_path) {
+  
+  # Add a column to determine label colors based on highlight_genes
+  heatmap_selection$label_color <- ifelse(heatmap_selection$variables %in% highlight_genes, "red", "black")
+  
+  breaks <- c(0, 1000, 2000, Inf)
+  
+  # Plot heatmap
+  plot<-ggplot(heatmap_selection, aes(x = foi, y = variables, fill = value)) +
+    geom_tile() +
+    scale_fill_gradientn(
+      colors = c("white","pink", "red", "darkred"), 
+      name = "MI"
+    )+
+    theme_minimal() +
+    labs(title = plot_name, x = "reference variables", y = "selected genes") +
+    theme(
+      axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1), # Rotate x-axis labels
+      axis.text.y = element_blank(), # Increase y-axis label size for readability
+    )+
+    geom_text(aes(label = variables, y = variables, x = 0, color = label_color), 
+              inherit.aes = FALSE, hjust = 0,size=1.5) +
+    scale_color_identity() # Use label_color as is without legend
+  
+  ggsave(output_path, plot = plot, width = 6, height = 10, bg = "white") 
+  
+  return(plot)
+}
+
+
+
+# return the list of selected variables as well as a heatmap 
+handle_MI_Selection <- function(mi_df,
+                                foi,
+                                topN,
+                                outdir,
+                                groupby=FALSE,
+                                topn=10,
+                                colorFeatures=c(""))
+{
+  # init to empty
+  selection_df <- data.frame()
+  if (groupby)
+  {
+     selection_df<-mi_df %>%
+       filter(foi %in% myfoi) %>% 
+       filter (!foi %in% c("RiboSMean","RiboLMean","mtMean")) %>%
+       group_by(foi) %>% 
+       arrange(desc(value)) %>%
+       slice_head(n = topn) %>%
+       ungroup() %>%
+       arrange(desc(value)) %>%
+       distinct(variables,.keep_all = TRUE) %>%
+       slice_head(n = topN)     
+  }
+  else
+  {
+    selection_df<-mi_df %>%
+      filter(foi %in% myfoi) %>% 
+      filter (!foi %in% c("RiboSMean","RiboLMean","mtMean")) %>%
+      arrange(desc(value)) %>%
+      slice_head(n = topN)
+  }
+  
+  selection <- selection_df %>% pull(variables)
+  write.csv(selection_df,file = file.path(outdir,paste0("Selection.",length(selection),factor(groupby),".csv")))
+  # heatmap 
+  file_name<-paste("heatmap",length(selection),factor(groupby),"highlighted.png",sep = ".")
+  heatmap_selection<-mi_df[(mi_df$foi %in% myfoi & mi_df$variables %in% selection),] 
+  outpath=paste0(outdir,"/",file_name)
+  MI_heatmap(heatmap_selection,colorFeatures,"",outpath)
+  
+  return(selection_df)
+}
+    
+
+
+Generate_miic_files <- function(
+    seurat_object,
+    selection,
+    metadata,
+    foi,
+    OUTDIR,
+    prefix_name,
+    groups=list(),
+    assert_consequence=c(),
+    contextual=c()
+)
+{
+  raw.count.matrix=seurat_object@assays$RNA$counts
+  selected_genes<-unique(selection) 
+  metadata_selected<-selected_genes[selected_genes %in% c(names(metadata))]
+  selected_genes<-selected_genes[!selected_genes %in% c(names(metadata))]
+  foi_genes = foi[!foi%in% names(metadata)]
+  # Add foi (genes) - make sure they are selected 
+  print("unique selected_genes size:")
+  print(length(selected_genes))
+  print("foi size (not foi metadata):")
+  print(length(foi_genes))
+  print("selected metadata:")
+  print(metadata_selected)
+  print(" selected_genes intersect foi :")
+  print(intersect(foi_genes,selected_genes))
+  
+  selected_genes<-unique(c(foi_genes,selected_genes))
+  nb_genes=length(selected_genes)
+  print("unique selected_genes + foi size:")
+  print(nb_genes)
+  
+  
+  
+  lapply(foi,function(foi_item){
+    # foi variable must be either a metadata or a gene 
+    stopifnot(foi_item %in% selected_genes | foi_item %in% names(metadata))
+  })
+  
+  # -----------dump csv matrix-------------------------------------------------
+  outfile_miic <- paste0(OUTDIR,"/",prefix_name,".",nb_genes,".csv")
+  
+  matrix.use <- as.data.frame(t(raw.count.matrix[selected_genes,]))
+  
+  # Add foi(metadata) and selected metadata
+  for (metadata_item in names(metadata)) {
+    if (metadata_item %in% foi | metadata_item %in% metadata_selected) {
+      
+      if (is.null(metadata[[metadata_item]])) { # discrete
+        matrix.use[[metadata_item]] <- seurat_object@meta.data[[metadata_item]]
+      } else { # categorical
+        matrix.use[[metadata_item]] <- as.factor(seurat_object@meta.data[[metadata_item]])
+      }
+      
+    }
+  }
+  
+  write.csv(matrix.use,outfile_miic,row.names = FALSE)
+  
+  
+  # --------- dump categorical order
+  outfile_contextual <- paste0(OUTDIR,"/",prefix_name,".",nb_genes,".st.txt")
+  category.order.use <- data.frame(var_names=colnames(matrix.use),
+                                   var_type = rep(1),
+                                   levels_increasing_order = rep(NA),
+                                   group = "gene",
+                                   is_contextual = rep(0),
+                                   is_consequence = rep(0))
+  
+  #define categorical vars and orders
+  for (metadata_item in names(metadata)) {
+    if (!is.null(metadata[[metadata_item]]) & metadata_item %in% colnames(matrix.use)) {
+      category.order.use[category.order.use$var_names == metadata_item, "var_type"] <- 0
+      category.order.use[category.order.use$var_names == metadata_item, "levels_increasing_order"] <- metadata[[metadata_item]]  # c("CTL,RES")
+    }
+  }
+  
+  #define contextual
+  if (length(contextual)>0)
+  {
+    for (item in contextual) {
+      category.order.use[category.order.use$var_names == item, "is_contextual"] <- 1
+    }
+  }
+  
+  #define consequences
+  if (length(assert_consequence)>0)
+  {
+    category.order.use<-category.order.use %>%
+      mutate(is_consequence = if_else(var_names %in% assert_consequence,1, is_consequence))
+    
+  }
+  
+  #define groups
+  category.order.use<-category.order.use %>%
+    mutate(group = if_else(var_names %in% names(metadata), "metadata", group))
+
+  if (length(groups)>0)
+  {
+    for (item in names(groups)) {  
+      category.order.use <- category.order.use %>%
+        mutate(group = if_else(var_names %in% groups[[item]], item, group))
+    }
+  } 
+  
+  write.table(category.order.use,outfile_contextual,row.names = FALSE)
+  
+}
